362251ae82bc7143bd721fa73de500550f0c27fa
Jonathan Chosson <jonathan.chosson@gmail.com>
2022-03-05 17:11:44 +0100

    [JS] Travail sur le caroussel :dizzy:

---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+/**
+ * Elements du DOM
+ */
+let caroussel = document.getElementById('contains');
+caroussel.innerHTML = ` <img src="./public/images/Pain1.jpg" alt="" class="caroussel__contains__image"">`
