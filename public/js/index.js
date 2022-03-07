d3ac00d133f17de7fb91507baee17b8a2fb42433
Jonathan Chosson <jonathan.chosson@gmail.com>
2022-03-07 17:18:35 +0100

    [JS] Finalisation du caroussel :dizzy:

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,44 @@
 /**
  * Elements du DOM
  */
-let caroussel = document.getElementById('contains');
-caroussel.innerHTML = ` <img src="./public/images/Pain1.jpg" alt="" class="caroussel__contains__image"">`
+let carousselImage = document.querySelectorAll('.caroussel__contains__image');
+i=0;
+/**
+ * Fonctions
+ */
+function ajoutSlideOUt(i,j){
+    carousselImage[i].classList.add('slide-out-left');
+    setTimeout(function test() {
+        carousselImage[i].classList.remove('slide-out-left')
+        carousselImage[i].classList.add('invisible')
+        carousselImage[j].classList.remove('invisible')
+        carousselImage[j].classList.add('slide-in-right')
+    }, 500)
+    setTimeout(function test() {
+        carousselImage[j].classList.remove('slide-in-right')
+    }, 1000)
+};
+
+function basculerAffichage(){
+    j = i + 1; 
+    if(j == carousselImage.length){
+            j = 0;
+    }
+
+    ajoutSlideOUt(i,j);
+    setTimeout(function modifieIndex(){i = j }, 1500)
+}
+
+function caroussel(){
+    setInterval(function basculer(){
+        basculerAffichage();
+    }, 3500)
+}
+
+
+
+/**
+ * Appel fonction
+ */
+
+caroussel();
