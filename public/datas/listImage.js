d0ade3b4ebde022dc21c36f1a3b6432f00c4abcc
Jonathan Chosson <jonathan.chosson@gmail.com>
2022-03-25 16:10:03 +0100

    [JS] Ajout d'onglet dynamique :dizzy:

---
+++ public/datas/listImage.js
@@ -0,0 +1,155 @@
+const listImage = [
+    {
+        cat : 'Pains',
+        name: 'Pains 1',
+        url: './public/images/realisations/Pain1.jpeg'
+    },
+    {
+        cat : 'Pains',
+        name: 'Pains 2',
+        url: './public/images/realisations/Pain2.jpeg'
+    },
+    {
+        cat : 'Pains',
+        name: 'Pains 3',
+        url: './public/images/realisations/Pain3.jpeg'
+    },
+    {
+        cat : 'Pains',
+        name: 'Pains 4',
+        url: './public/images/realisations/Pain4.jpeg'
+    },
+    {
+        cat : 'Pains',
+        name: 'Pains 5',
+        url: './public/images/realisations/Pain5.jpeg'
+    },
+    {
+        cat : 'Pains',
+        name: 'Pains 6',
+        url: './public/images/realisations/Pain6.jpeg'
+    },
+    {
+        cat : 'Viennoiseries',
+        name: 'Viennoiseries 1',
+        url: './public/images/realisations/Viennoiseries1.jpeg'
+    },
+    {
+        cat : 'Viennoiseries',
+        name: 'Viennoiseries 2',
+        url: './public/images/realisations/Viennoiseries2.jpg'
+    },
+    {
+        cat : 'Viennoiseries',
+        name: 'Viennoiseries 3',
+        url: './public/images/realisations/Viennoiseries3.jpeg'
+    },
+    {
+        cat : 'Viennoiseries',
+        name: 'Viennoiseries 4',
+        url: './public/images/realisations/Viennoiseries4.jpeg'
+    },
+    {
+        cat : 'Viennoiseries',
+        name: 'Viennoiseries 5',
+        url: './public/images/realisations/Viennoiseries5.jpeg'
+    },
+    {
+        cat : 'Viennoiseries',
+        name: 'Viennoiseries 6',
+        url: './public/images/realisations/Viennoiseries6.jpeg'
+    },
+    {
+        cat : 'Gâteux de voyage',
+        name: 'Gâteux de voyage 1',
+        url: './public/images/realisations/gdv1.jpeg'
+    },
+    {
+        cat : 'Gâteux de voyage',
+        name: 'Gâteux de voyage 2',
+        url: './public/images/realisations/gdv2.jpeg'
+    },
+    {
+        cat : 'Gâteux de voyage',
+        name: 'Gâteux de voyage 3',
+        url: './public/images/realisations/gdv3.jpeg'
+    },
+    {
+        cat : 'Gâteux de voyage',
+        name: 'Gâteux de voyage 4',
+        url: './public/images/realisations/gdv4.jpeg'
+    },
+    {
+        cat : 'Gâteux de voyage',
+        name: 'Gâteux de voyage 5',
+        url: './public/images/realisations/gdv5.jpeg'
+    },
+    {
+        cat : 'Gâteux de voyage',
+        name: 'Gâteux de voyage 6',
+        url: './public/images/realisations/gdv6.jpeg'
+    },
+    {
+        cat : 'Patisserie boulangère',
+        name: 'Patisserie boulangère 1',
+        url: './public/images/realisations/pdv1.jpeg'
+    },
+    {
+        cat : 'Patisserie boulangère',
+        name: 'Patisserie boulangère 2',
+        url: './public/images/realisations/pdv2.jpeg'
+    },
+    {
+        cat : 'Patisserie boulangère',
+        name: 'Patisserie boulangère 3',
+        url: './public/images/realisations/pdv3.jpeg'
+    },
+    {
+        cat : 'Patisserie boulangère',
+        name: 'Patisserie boulangère 4',
+        url: './public/images/realisations/pdv4.jpeg'
+    },
+    {
+        cat : 'Patisserie boulangère',
+        name: 'Patisserie boulangère 5',
+        url: './public/images/realisations/pdv5.jpeg'
+    },
+    {
+        cat : 'Patisserie boulangère',
+        name: 'Patisserie boulangère 6',
+        url: './public/images/realisations/pdv6.jpeg'
+    },
+    {
+        cat : 'Snacking',
+        name: 'Snacking 1',
+        url: './public/images/realisations/Snacking1.jpg'
+    },
+    {
+        cat : 'Snacking',
+        name: 'Snacking 2',
+        url: './public/images/realisations/Snacking2.jpeg'
+    },
+    {
+        cat : 'Snacking',
+        name: 'Snacking 3',
+        url: './public/images/realisations/Snacking3.jpeg'
+    },
+    {
+        cat : 'Snacking',
+        name: 'Snacking 4',
+        url: './public/images/realisations/Snacking4.jpeg'
+    },
+    {
+        cat : 'Snacking',
+        name: 'Snacking 5',
+        url: './public/images/realisations/Snacking5.jpeg'
+    },
+    {
+        cat : 'Snacking',
+        name: 'Snacking 6',
+        url: './public/images/realisations/Snacking6.jpeg'
+    }
+    
+]
+
+export default listImage
--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,21 @@
+import listImage from '../datas/listImage.js';
+
+
 /**
  * Elements du DOM
  */
 let carousselImage = document.querySelectorAll('.caroussel__contains__image');
-i=0;
+let listCategorie = document.querySelectorAll('.ulRea__li');
+let realisations = document.getElementById('realisations');
+let i=0;
 /**
  * Fonctions
+ */
+
+/**
+ * Fonction qui toogle l'affichage et l'animation sur les images
+ * @param {number} i index de l'image en cours
+ * @param {number} j index de l'image suivante
  */
 function ajoutSlideOUt(i,j){
     carousselImage[i].classList.add('slide-out-left');
@@ -19,8 +30,11 @@ function ajoutSlideOUt(i,j){
     }, 1000)
 };
 
+/**
+ * Gère l'indentation de J
+ */
 function basculerAffichage(){
-    j = i + 1; 
+    let j = i + 1; 
     if(j == carousselImage.length){
             j = 0;
     }
@@ -29,16 +43,67 @@ function basculerAffichage(){
     setTimeout(function modifieIndex(){i = j }, 1500)
 }
 
+/**
+ * appel de la fonction qui idente J
+ */
 function caroussel(){
     setInterval(function basculer(){
         basculerAffichage();
     }, 3500)
 }
 
+/**
+ * Affiche la classe active de base
+ */
+afficheRealisation(Array.from(listCategorie).find(cat => cat.classList.contains('active')).outerText)
+
+/**
+ * Listenner sur les clic de catégorie
+ */
+listCategorie.forEach(cat => {
+    cat.addEventListener('click', function (){
+        changeActive(cat.outerText)
+        afficheRealisation(cat.outerText)
+    })
+});
+
+/**
+ * Change la class active sur les onglets
+ * @param {string} catActive categorie que l'on souhaite activer
+ */
+function changeActive(catActive){
+    Array.from(listCategorie).map(cat => {
+        if(cat.outerText === catActive){
+            cat.classList.add('active')
+        }else{
+            cat.classList.remove('active')
+        }
+    })
+}
+
+/**
+ * Affiche les images pour une catégorie choisie
+ * @param {string} catActive categorie que l'on souhaite activer
+ */
+function afficheRealisation(catActive){
+    realisations.innerHTML = ''
+    let images  = listImage.filter(image => image.cat === catActive)
+    images.forEach(image => {
+        realisations.innerHTML += composantImage(image) ;
+    });
+}
+
+/**
+ * Retourne le composant image
+ * @param {object} image Objet image fournis par @see {afficheRealisation()}
+ * @returns html composant pour l'image
+ */
+function composantImage(image){
+    return `<img src="${image.url}" alt="${image.name}" class="realisations__img">`;
+}
 
 
 /**
- * Appel fonction
+ * Appel fonction Générale
  */
-
 caroussel();
